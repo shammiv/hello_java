7b16b59408f6a5ecf4ffe3bffa3fd981762c58bd
shammiv <61626457+shammiv@users.noreply.github.com>
2020-06-11 12:54:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
-public class Hello {
- public static void main (string[] args) {
-  //print "Hello,world" in the terminal window
-    System.out.prinln("Hello , world");
-	}
-  
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
